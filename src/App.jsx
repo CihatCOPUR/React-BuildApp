412c9859843708982aaa3144200e7f7c626a02b3
Cihat <109276013+CihatCOPUR@users.noreply.github.com>
2024-07-04 21:54:18 +0300

    projectStart

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bulma/css/bulma.min.css';
+
+function App() {
+
+  return (
+    <>
+  <h1>Kat irtifakı</h1>
+    </>
+  )
+}
+
+export default App
